5822ce4b56633fc1a5e83b3d5ad3b717414f59be
yasinbaghban <ysnbgn.bgn@gmail.com>
2021-09-04 06:31:57 +0330

    anagram task

--- php/kata/anagram.php
+++ php/kata/anagram.php
@@ -15,8 +15,12 @@ function is_anagram(string $s1, string $s2): bool
 function is_anagram2(string $s1, $s2): bool
 {
     // Home work: complete the function using count_chars bultin function
+    $s1 = strtolower(str_replace(' ', '', $s1));
+    $s2 = strtolower(str_replace(' ', '', $s1));
+
+    return count_chars($s1, 1) === count_chars($s2, 1);
 }
 
 
-// var_dump(is_anagram('Public relations', 'Capture billions'));
+ //var_dump(is_anagram2('Public relations', 'Capture billions'));
 
