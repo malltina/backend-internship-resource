b44c5b8f88e2c305d021afad61a2a8672e4dcead
yasinbaghban <ysnbgn.bgn@gmail.com>
2021-09-04 07:45:43 +0330

    plaindrome task

--- php/kata/plaindrome.php
+++ php/kata/plaindrome.php
@@ -5,4 +5,13 @@ function is_plaindrome(string $s1, string $s2): bool
     return strrev($s1) === $s2;
 }
 
+function is_plaindrome2(string $s1, string $s2): bool
+{
+    if (substr($s1, 0, 1) == substr($s2, (strlen($s2) - 1), 1)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
 // var_dump(is_plaindrome('loop', 'pool'));
