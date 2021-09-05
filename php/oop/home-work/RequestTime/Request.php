e76e82ec5c3ec9fc1c62818d0149709cf8e87df4
pshko <aram.khorsandi@gmail.com>
2021-09-05 16:57:33 +0430

    request-time

---
+++ php/oop/home-work/RequestTime/Request.php
@@ -0,0 +1,36 @@
+<?php
+class Request
+{
+    public string $type;
+    public int  $getCounter = 0;
+    public int $postCounter = 0;
+
+    public function get($url) :void
+    {
+        $this->getCounter++;
+    }
+
+    public function post($url) :void
+    {
+        $this->postCounter++;
+    }
+
+    public function countOfCalls() :int
+    {
+        return $this->getCounter + $this->postCounter;
+    }
+
+    public function countOfCallFor($type) :int
+    {
+        if ($type == 'get') {
+            return $this->getCounter;
+        }
+        elseif ($type == 'post')
+        {
+            return  $this->postCounter;
+        }
+        else{
+            throw new Exception('type is invalid!');
+        }
+    }
+}
