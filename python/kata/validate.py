3cf6cd516d0a5e987c461635fe896a0860b20cf0
finite_ali <ali.hosseini7785@gmail.com>
2021-09-06 23:52:12 +0430

    validate

--- python/kata/validate.py
+++ python/kata/validate.py
@@ -1,6 +1,39 @@
-def validate(s):
-    """ Write a code to check whether a given string contains a capital letter, 
-    a lower case letter, a number and a minimum length. Minimum length must be grader than 5 """
+def validate(string):
+    problems = []
+    # validate number
+    if any(i.isdigit() for i in string) is False:
+        problems.append('The word must also contain a number')
+    # validate uppercase
+    is_upper = False
+    for letter in string:
+        if letter.isupper():
+            is_upper = True
+            break
+        else:
+            is_upper = False
+    if is_upper is False:
+        problems.append('The word must also contain a uppercase')
+
+    # validate lowercase
+    is_lower = False
+    for letter in string:
+        if letter.islower():
+            is_lower = True
+            break
+        else:
+            is_lower = False
+    if is_lower is False:
+        problems.append('The word must also contain lowercase')
+
+    # validate len
+    if len(string) < 5:
+        problems.append('Minimum string length must be greater than 5')
+
+    if len(problems) == 0:
+        return['Valid string']
+    else:
+        return problems
+
 
 
 print(validate('D3veloper'))
