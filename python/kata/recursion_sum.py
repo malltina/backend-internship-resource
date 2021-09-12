08a9ac32ddb6826b80cd69daece79b07fefe59c6
finite_ali <ali.hosseini7785@gmail.com>
2021-09-12 16:54:25 +0430

    recursion_sum

--- python/kata/recursion_sum.py
+++ python/kata/recursion_sum.py
@@ -1,7 +1,17 @@
-def sum(numbers):
-    """ Write a code to sum numbers of list
-    Note: Use recursion """
+def sum1(lst):
+    total = 0
+    for element in lst:
+        if type(element) is list:
+            total = total + sum1(element)
+        else:
+            total = total + element
+    return total
 
 
-assert sum([1, 2, [3,4], [5,[6, 7]]]) == 28
-assert sum([1, 1, [1,1], [1,[1, [[1,[1,1,1,[1,1],1]],1,1], 1]]]) == 16
+# print("Sum is:", sum1([[1, 2], [3, 4]]))
+
+# assert sum([1, 2, [3, 4], [5, [6, 7]]]) == 28
+# assert sum([1, 1, [1, 1], [1, [1, [[1, [1, 1, 1, [1, 1], 1]], 1, 1], 1]]]) == 16
+
+print(sum1([1, 2, [3, 4], [5, [6, 7]]]))
+# print(sum1([1, 1, [1, 1], [1, [1, [[1, [1, 1, 1, [1, 1], 1]], 1, 1], 1]]]))
