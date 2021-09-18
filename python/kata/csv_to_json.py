85e75871613fada492ba4f7b95e79ed10dac9412
finite_ali <ali.hosseini7785@gmail.com>
2021-09-18 22:35:56 +0430

    csv to json

--- python/kata/csv_to_json.py
+++ python/kata/csv_to_json.py
@@ -1,7 +1,25 @@
-def csv_to_json(csv_filename):
-    """ Write a code to read csv file content and store as json us-states.json, 
-    finally return csv file content as list """
+import csv
+import json
 
 
-print(csv_to_json('resource/usa-states.csv'))
+def csv_to_json(csv_path, json_path):
+    jason = []
+    with open(csv_path, encoding='utf-8') as csv_file:
+        # csv.DictReader make a dict type of our csv file elements
+        csv_reader = csv.DictReader(csv_file)
+
+        for row in csv_reader:
+            jason.append(row)
+    with open(json_path, 'w', encoding='utf-8') as json_file:
+        # json.dumps() function converts a Python object into a json string
+        json_string = json.dumps(jason, indent=4)
+        json_file.write(json_string)
+
+    return json_string
+
+
+csv_file_path = input()
+json_file_path = input()
+
+print(csv_to_json(csv_file_path, json_file_path))
 # Expected Result: [{"StateCode": "AK", "StateName": "Alaska", "IsRealState": True, "StateDate": "1819-12-14"}, ...]
